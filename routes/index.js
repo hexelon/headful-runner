c3be516ee84ab12ae3801f2ed494107c65c32b80
Kfir Ben-Ami <kfirba2@gmail.com>
2020-10-26 17:12:55 +0200

    run browser in incognito mode in a single page. also fix window size

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,20 @@ const puppeteer = require('puppeteer-core');
 const router = express.Router();
 
 /* GET home page. */
-router.post('/cookies',  async function (req, res, next) {
-  const { cookies, url } =  req.body;
+router.post('/cookies', async function(req, res, next) {
+  const { cookies, url } = req.body;
 
   const browser = await puppeteer.launch({
     executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
     headless: false,
+    devtools: process.env.DEVTOOLS || false,
+    defaultViewport: null,
+    args: [
+      '--start-maximized',
+      '--incognito',
+    ],
   });
-  const context = await browser.createIncognitoBrowserContext();
-  const page = await context.newPage();
+  const page = (await browser.pages())[0];
   await page.setCookie(...cookies);
   await page.goto(url);
 });
